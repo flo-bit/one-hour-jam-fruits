f401ab94c13c8283870096f9ecc9a83e6e71f9de
Florian <45694132+flo-bit@users.noreply.github.com>
2025-05-17 20:03:57 +0200

    build

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  // Set base path for GitHub Pages
+  base: '/one-hour-jam-fruits/',
+  
   // Disable type checking during build
   build: {
     // These settings help make Vite ignore TypeScript errors during build
